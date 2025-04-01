1e4a3fae19becebec772987a6b2f9e955e0f09ac
ErickMCabral <erickmarques3@gmail.com>
2025-04-01 18:25:38 -0300

    refactor: :sparkles: introduce a new structure for component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { FooterBody } from "./FooterBody";
+import { FooterBoton } from "./FooterBotom";
+import { FooterHeader } from "./FooterHeader";
+import { FooterRoot } from "./FooterRoot";
+
+export const Footer = {
+  Root: FooterRoot,
+  Header: FooterHeader,
+  Botom: FooterBoton,
+  Body: FooterBody,
+}
